dd95ffded61bfb54953fbd2dab1736da2af29e5b
Jelldo <astrokwak@gmail.com>
2019-06-21 00:09:33 +0900

    box_vertex

---
+++ test_models/BoxVertexColors/boxvertexcolors_vertex.glsl
@@ -0,0 +1,15 @@
+﻿#version 120                  // GLSL 1.20
+
+uniform mat4 u_PVM;
+uniform mat4 u_M;
+
+attribute vec3 a_color;
+attribute vec3 a_position;    // per-vertex position (per-vertex input)
+
+varying vec3 v_color;
+
+void main()
+{
+  gl_Position   = u_PVM * vec4(a_position, 1.0f);
+  v_color = a_color;
+}
